07da5f1197be29f16df8d909219bd12177da7dc5
Prasad <prasadgawade2004@gmail.com>
2025-08-04 11:57:26 +0530

    this is tns session program

---
+++ TNS Session/src/com/college/day1.java
@@ -0,0 +1,75 @@
+package com.college;
+
+public class day1 {
+	public static void main(String[] args) {
+		
+	  //  valid indentifier in variable 
+		int $num=12;
+		System.out.println("value of the variable= "+$num);
+		
+	// Datatype   VarName    Data
+		String StudentName="Prasad";
+		System.out.println("value of the name= "+StudentName);
+		
+	// different types of datatypes
+		
+	//int	
+		int value1=9/2;
+		System.out.println("value1= "+value1);
+		
+	//float
+		float value2=100f/3f;
+		System.out.println("value2= "+value2);
+		
+	//double	
+		double value3=100d/3d;
+		System.out.println("value3= "+value3);
+		
+	//expression	
+		int radius=10;
+		double Area=3.14*radius*radius;
+		System.out.println("Area= "+Area);
+		
+	//char
+		char ch='P';
+		System.out.println("ch= "+ch);
+		
+	//assining num to char
+		char ch1=65;
+		System.out.println("ch1= "+ch1);
+		
+	 // bytes takes 1 byte	
+		byte byteMax=126;
+		byte byteMin=-128;
+		System.out.println("Min Range is= "+byteMin+" and Max range is= "+byteMax);
+		
+	// short 2 byte
+		short shortMax=32767;
+		short shortMin=-32768;
+		System.out.println("Min Range of short= "+shortMax+" and Max range of short= "+shortMin);
+
+	// integer 4 byte
+		int intMin=-2147483648;
+		int intMax=2147483647;
+		System.out.println("Max Range of int= "+intMax+" and Min range of int= "+intMin);
+
+		
+	// long
+		long longMax=9223372036854775807l;
+		long longMin=-9223372036854775808l;
+		System.out.println("Max Range of long= "+longMax+" and Min range of long= "+longMin);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
